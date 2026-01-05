a36871aaa6be68324a18f145aeba3ca314d82f2b
Serhii Voznyi <serhiivoznyi@gmail.com>
2026-01-05 13:27:24 +0100

    Initial Flutter baseline

---
+++ src/flutter/yum_or_yuck/android/app/src/main/kotlin/com/example/yum_or_yuck/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.yum_or_yuck
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
--- src/С#/SandBox/obj/Debug/net9.0/SandBox.AssemblyInfo.cs
+++ src/С#/SandBox/obj/Debug/net9.0/SandBox.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SandBox")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f47a3a15f284bb2486211ffe06d9635cedc08689")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c74835d60ff996c861228faff37e5aa60f9531d")]
 [assembly: System.Reflection.AssemblyProductAttribute("SandBox")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SandBox")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
