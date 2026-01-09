5a621d05e98dd1763a334ed8858efd313a061d49
Serhii Voznyi <serhiivoznyi@gmail.com>
2026-01-09 19:09:30 +0100

    Playing with themes

--- src/С#/SandBox/obj/Debug/net9.0/SandBox.AssemblyInfo.cs
+++ src/С#/SandBox/obj/Debug/net9.0/SandBox.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SandBox")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7355ec82df51636f33ee0eb986afd5328a063df8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4999fd9546532b09f52653dfb14a829cd9273511")]
 [assembly: System.Reflection.AssemblyProductAttribute("SandBox")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SandBox")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
